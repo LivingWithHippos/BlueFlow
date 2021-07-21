705a509ced4f041d39f802db6848247259e52ab4
LivingWithHippos <LivingWithHippos@users.noreply.github.com>
2021-07-21 15:06:19 +0200

    updated kotlin dependency

--- buildSrc/src/main/java/LibraryDependency.kt
+++ buildSrc/src/main/java/LibraryDependency.kt
@@ -22,7 +22,7 @@ private object LibraryVersion {
 
 // Versions consts that are used across libraries and Gradle plugins
 object CoreVersion {
-    const val KOTLIN = "1.4.30"
+    const val KOTLIN = "1.5.21"
     const val KTLINT = "0.36.0"
     const val NAVIGATION = "2.3.3"
 }
